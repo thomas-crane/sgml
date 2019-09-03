417cf5814e62ef3b925ac38a136cdecb73bc0911
thomas-crane <thomasgcrane@gmail.com>
2019-09-03 22:31:55 +1000

    feat(syntax): add text span

---
+++ src/syntax/text-span.ts
@@ -0,0 +1,12 @@
+export class TextSpan {
+  constructor(
+    /**
+     * The start index of this text span.
+     */
+    readonly start: number,
+    /**
+     * The length of this text span.
+     */
+    readonly length: number,
+  ) { }
+}
