2cac84173ead4955396e2cee0a1600affee85c06
thomas-crane <thomasgcrane@gmail.com>
2019-09-11 22:39:49 +1000

    chore(diagnostics): add source to diagnostics

--- src/diagnostics/diagnostic.ts
+++ src/diagnostics/diagnostic.ts
@@ -1,7 +1,9 @@
 import { TextSpan } from '../syntax/text-span';
+import { Source } from '../text/source';
 import { DiagnosticLevel } from './diagnostic-level';
 
 export interface Diagnostic {
+  source: Source;
   level: DiagnosticLevel;
   span: TextSpan;
   message: string;
